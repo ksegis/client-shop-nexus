c37e6f4f7874b98cc01f5c149438d5e47686ff75
kevinshelton-2708 <kevin.shelton@egisdynamics.com>
2025-06-09 23:22:44 -0500

    css changes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -88,3 +88,4 @@ const Header = ({ portalType }: HeaderProps) => {
   );
 };
 export default Header;
+
--- src/components/layout/header/DesktopNavigation.tsx
+++ src/components/layout/header/DesktopNavigation.tsx
@@ -115,4 +115,3 @@ export const DesktopNavigation = ({ links, currentPath }: DesktopNavigationProps
     </NavigationMenu>
   );
 };
-
